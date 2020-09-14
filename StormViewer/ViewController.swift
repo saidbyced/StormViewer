5919059fdc87664d2881c7b43b150e1a15b4ad5d
Chris Eadie <chris.eadie@outlook.com>
2020-09-14 12:45:56 +0100

    Add picture filenames into table cells to be used as links to image displays

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -28,4 +28,14 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
 }
