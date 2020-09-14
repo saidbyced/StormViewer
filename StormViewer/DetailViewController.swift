ce142724aa85a14313b99f19c0ae7af4b7bb880a
Chris Eadie <chris.eadie@outlook.com>
2020-09-14 12:47:04 +0100

    Add new view controller to handle displaying image selected by filename

---
+++ StormViewer/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Chris Eadie on 14/09/2020.
+//  Copyright © 2020 Eadie Designs. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
