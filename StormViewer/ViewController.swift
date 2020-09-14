6a384b48028069bb627ceaf47c3537ffa31d0a0f
Chris Eadie <chris.eadie@outlook.com>
2020-09-14 13:33:37 +0100

    Challenge 2: sort images so they are displayed in alphanumeric order

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -25,6 +25,8 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
+        
+        pictures = pictures.sorted()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
