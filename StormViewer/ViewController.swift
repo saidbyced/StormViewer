84e8b5874dc25491447b60eefa0146de18ff487d
Chris Eadie <chris.eadie@outlook.com>
2020-09-14 13:26:00 +0100

    Correct navigation titles sizes to preferential design aesthetics

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let fileManager = FileManager.default
         let path = Bundle.main.resourcePath!
